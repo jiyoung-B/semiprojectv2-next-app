23728fece32a7f47d76883222872bac80492dd66
jiyoung-B <catgirl0313@naver.com>
2023-03-13 17:42:20 +0900

    MariaDB Board 클래스 구현 10 - 코드 리팩터링

--- models/Utils.js
+++ models/Utils.js
@@ -9,4 +9,18 @@ const check_captcha = async (response) => {
 
 };
 
-module.exports = {check_captcha};
+const handleInput = (setInput, e) => {
+    setInput(e.target.value);  // 함수자체를 매개변수로 넘긴다.
+};
+
+const process_submit = async (url, data) => {
+    const cnt = fetch(url, {
+        method: 'POST', mode: 'cors', // 주소 같으면 cors 필요없음
+        body: JSON.stringify(data),
+        headers: {'Content-Type': 'application/json'},
+    }).then(res => res.json());
+
+    return (await cnt).cnt;
+};
+
+module.exports = {check_captcha, handleInput, process_submit};
--- pages/board/write.js
+++ pages/board/write.js
@@ -1,32 +1,6 @@
 import axios from "axios";
-import {useState} from "react";
+import {check_captcha, handleInput, process_submit } from "../../models/Utils";
 
-// export async function getServerSideProps(ctx) {
-//     let bno = ctx.query.bno;
-//     let url = `http://localhost:3000/api/board/view?bno=${bno}`;
-//     const res = await axios.get(url);  // axiox
-//     const board = await res.data[0];
-//     console.log(board);
-//
-//     return { props : {board} }
-//
-const check_captcha = async (response) => {
-    let url = '/api/board/recaptcha?response=' + response;
-    const data = axios.get(url).then(data => data.data);
-    console.log((await data).success);
-
-    return (await data).success;
-
-};
-const process_write = async (data) => {
-    const cnt = fetch('/api/board/write', {
-        method: 'POST', mode: 'cors', // 주소 같으면 cors 필요없음
-        body: JSON.stringify(data),
-        headers: {'Content-Type': 'application/json'}
-    }).then(res => res.json());
-
-    return (await cnt).cnt;
-};
 
 export default function Write() {
     const [title, setTitle] = useState('');
@@ -37,31 +11,31 @@ export default function Write() {
 
     const handlewrite = async () => {
         if (grecaptcha.getResponse() // 이미지캡챠 클릭하면
-            && check_captcha(grecaptcha.getResponse())) { // 클릭한게 정상이라면
+            && await check_captcha(grecaptcha.getResponse())) { // 클릭한게 정상이라면
             //글쓰기 작업 진행
             const data = {title: title, userid: userid, contents: contents};
-            if (await process_write(data) > 0) {
+            if (await process_submit('/api/board/write', data) > 0) { // process_write => process_submit으로 Utils에 모듈화
                 location.href = '/board/list'
             }
         }
 
     };
-    const handleTitle = (e) => {setTitle(e.target.value)};
-    const handleContents = (e) => {setContents(e.target.value)};
+
     return (
         <main>
+            <script src="https://www.google.com/recaptcha/api.js" async defer></script>
             <div id="main">
                 <h2>게시판 새글쓰기</h2>
                 <form name="write" id="writefrm">
                     <div><label htmlFor="title">제목</label>
-                        <input type="text" name="title" id="title" onChange={handleTitle} /></div>
+                        <input type="text" name="title" id="title" onChange={e => handleInput(setTitle,e)} /></div>
 
                     <div><label htmlFor="uid">작성자</label>
                         <input type="text" name="uid" id="uid"
                                value={userid} readOnly /></div>
 
-                    <div><label htmlFor="contents" className="dragup">본문</label>
-                        <textarea name="contents" id="contents" onChange={handleContents}
+                    <div><label htmlFor="contents" className="drgup">본문</label>
+                        <textarea name="contents" id="contents" onChange={e => handleInput(setContents,e)}
                                   rows="7" cols="55"></textarea></div>
 
                     <div><label></label>
